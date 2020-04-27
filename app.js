bbb7690a53892c66fd832c5e690e8b18139af3dc
ja-br <letstalk@jamesbrooks.tech>
2020-04-27 14:34:10 -0400

    Made DOM query selectors, min and max value range is displayed

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert(1)
+let min = 1,
+    max = 15,
+    winCondition = 2,
+    guessesLeft = 3
+
+const game = document.querySelector('#game'),
+    minNum = document.querySelector('.min-num'),
+    maxNum = document.querySelector('.max-num'),
+    guessBtn = document.querySelector('#guess-btn'),
+    guessInput = document.querySelector('#guess-input'),
+    message = document.querySelector('.message')
+
+minNum.textContent = min
+maxNum.textContent = max
